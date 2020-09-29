e699c6a9817f43cb042848a9c153cb2d220108c7
tsdhr1 <gougewh@sina.com>
2020-09-29 21:54:55 +0800

    优化了下上传歌曲

--- src/controllers/songs.js
+++ src/controllers/songs.js
@@ -2,6 +2,9 @@ const Song = require('../models/Song');
 const Square = require('../models/Square');
 const fse = require('fs-extra')
 const Sequelize = require('sequelize');
+const FileType = require('file-type');
+const mm = require('music-metadata');
+const uuid = require('node-uuid');
 
 const Op = Sequelize.Op;
 const getFileName = path =>{
@@ -12,9 +15,33 @@ const getFileName = path =>{
 class SongCtl {
   // 上传歌曲文件
   async uploadSong(ctx) {
-    const { path } = ctx.req.files['file']
-    ctx.body = path
+    const file = ctx.req.files['file']
+    // 我们拿到文件信息，通过文件夹名给文件夹重新命名。然后解析文件，拿到他的封面，歌手，专辑啥的信息
+    const reqFileType = await FileType.fromFile(file.path)
+    const newPath = `${file.path}.${reqFileType.ext}`
+    await fse.move(file.path, newPath)
+    const { common } = await mm.parseFile(newPath)
+    if (common) {
+      const uid = uuid.v1()
+      await fse.writeFile(`./${uid}${common.picture[0].description}`,common.picture[0].data)
+      const resBody = {
+        path: newPath,
+        songName: common.title || '',
+        singer: common.artist || '',  // 待优化，可以操作数组 artists
+        album: common.album || ''
+      }
+      if (common.picture[0]) {  // 如果存在的话，代表歌曲文件有封面
+        await fse.writeFile(`/data/cover-store/${uid}${common.picture[0].description || '.jpg'}`,common.picture[0].data)
+        resBody.coverPath = `http://49.233.185.168:3003/cover-store/${uid}${common.picture[0].description || '.jpg'}`
+      }
+      ctx.body = resBody
+    } else {
+      ctx.body = {
+        path: newPath
+      }
+    }
   }
+
   // 上传歌曲封面
   async uploadCover(ctx) {
     const file = ctx.req.files['file']
@@ -50,12 +77,28 @@ class SongCtl {
     })
     ctx.body = result
   }
-
+  // 获取我的音乐
   async list(ctx) {
     const songList = await Song.findAll({
       attributes: ['songName', 'singer', 'coverPath', 'songPath', 'album', 'id'],
       where: {
-        songName: ctx.params.key
+        [Op.or]: [  // 根据搜索值，不仅要搜索歌曲名，还要搜索歌手，还要搜索专辑。都是模糊查询
+          {
+            songName: {
+              [Op.like]: '%' + ctx.params.key + '%'
+            }
+          },
+          {
+            singer: {
+              [Op.like]: '%' + ctx.params.key + '%'
+            }
+          },
+          {
+            album: {
+              [Op.like]: '%' + ctx.params.key + '%'
+            }
+          }
+        ]
       },
     })
     ctx.body = songList
