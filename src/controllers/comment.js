dbc429b33b96fd03350fa7f56c4d09782ecd282f
senlin <gougewh@sina.com>
2020-11-19 20:09:36 +0800

    debug

--- src/controllers/comment.js
+++ src/controllers/comment.js
@@ -1,6 +1,30 @@
 const { Comment, User, Square } = require('../models/index');
 
 class CommenCtr {
+  async test() {
+    ctx.body = {
+      errCode: 0,
+      errMsg: "",
+      data: {
+        banner: [{
+          bookId: 743917716766795,
+          img: "http://49.233.185.168:3003/image-store/cover/34f5c8d0-1f80-11eb-8168-b3cd37fd9728YA.jpg",
+          jumpCont: "",
+          jumpType: 0,
+          position: 0,
+        }],
+        man: [{
+          bookAuthor: "—",
+          bookCategoryName: "都市",
+          bookCover: "http://49.233.185.168:3003/image-store/cover/e0938c60-200a-11eb-acd7-25f73ece9cd5.jpg",
+          bookId: 781341098508289,
+          bookName: "超燃狂少",
+          bookSummary: "我叫王浩，大学毕业后，找不到好工作，混了三年，一事无成。 正当自己处于人生低谷的时候，一个意外的电话，却让我的命运出现了拐点。"
+        }],
+        women: []
+      }
+    }
+  }
   // 拿到该广场动态的全部评论，然后根据rootCommentId设置层级。
   async list(ctx) {
     const { squareId } =  ctx.params
--- src/routes/comment.js
+++ src/routes/comment.js
@@ -1,9 +1,10 @@
 const Router = require('koa-router')
-const { list, comment } = require('../controllers/comment.js')
+const { list, comment, test } = require('../controllers/comment.js')
 const loginCheck = require('../middlewares/loginChecks')
 const router = new Router({prefix:'/comment'})
 
 router.get('/:squareId', list)
 router.post('/:squareId', loginCheck, comment)
+router.post('/data', loginCheck, test) // 测试,稍后删除
 
 module.exports = router
